eeaf8b41ecf5df1d017e44629aa18dc956940aa0
Norbertas <nlapenas@gmail.com>
2024-02-16 09:11:34 +0000

    write_tours_data() immplamented and tested

--- TourMockApp/TourMockApp.py
+++ TourMockApp/TourMockApp.py
@@ -1,1 +1,22 @@
 
+TOUR_DATA_PATH: str = "Tours.csv"
+TOUR_HEADERS:list[str] = ["TourID", "Destination", "Duration", "Price per Person", "Min # Walkers", "Max # Walkers"]
+
+TEST_DATA = [["Tour1", "Sugarloaf", "3", "10.00", "5", "12"], ["Tour2", "Balalaika", "2", "8.00", "6", "14"]]
+
+def write_tours_data(data: list[list[str]]):
+    
+    temp:list[str] = []
+    #parse the data into line strings
+    temp.append(",".join(TOUR_HEADERS)+"\n") # HEADERS
+    for elements in data:
+        temp.append(",".join(elements)+"\n") # LINE DATA
+    try:
+        with open(TOUR_DATA_PATH, "w") as file:
+            file.writelines(temp)
+    except Exception as ex:
+        print(ex)
+    
+#testing write function
+write_tours_data(TEST_DATA)
+
