e315e35fca84a0b0f9ab28c1195f9b67d01f19c0
Norbertas <nlapenas@gmail.com>
2024-02-16 10:42:56 +0000

    renamed tours_data  and tours_data to lowercase since they are not constants

--- TourMockApp/TourMockApp.py
+++ TourMockApp/TourMockApp.py
@@ -5,13 +5,13 @@ TOUR_DATA_PATH: str = "Tours.csv"
 TOUR_HEADERS:list[str] = ["TourID", "Destination", "Duration", "Price per Person", "Min # Walkers", "Max # Walkers"]
 #Dummy Data
 TOURS_TEST_DATA = [["Tour1", "Sugarloaf", "3", "10.00", "5", "12"], ["Tour2", "Balalaika", "2", "8.00", "6", "14"]]
-TOURS_DATA = []
+tours_data = []
 
 GUIDES_DATA_PATH: str = "Guides.csv"
 GUIDES_HEADERS:list[str] = ["GuideID", "Name", "Basic Rate(Hourly)", "Rate per Walker"]
 #Dummy Data
 GUIDES_TEST_DATA = [["Guide7", "Barbara", "12.50", "1.25"], ["Guide72", "Eddie", "15.00", "2"]]
-GUIDES_DATA = []
+tours_data = []
 
 
 
@@ -62,7 +62,7 @@ def read_tour_data():
     '''
     
     #had a bug where the data was duplicating 
-    TOURS_DATA.clear() ## purgin all old data
+    tours_data.clear() ## purgin all old data
     
     try:
         with open(TOUR_DATA_PATH, "r") as file:
@@ -71,42 +71,49 @@ def read_tour_data():
             
             # lets ignore empy lines
             if line != "\n":
-                TOURS_DATA.append(line.split(","))
+                tours_data.append(line.split(","))
     except Exception as ex:
         print(ex)
         
-
-def update_tour_data(id_to_update: str, index_to_update: int, value_to_add: str):
+def read_guides_data():
     '''
-    Updates tour data
-    id_to_update = the unique id of the tour
-    index_to_update = the field u want to update 
-        0 = "TourID", 
-        1 = "Destination", 
-        2 = "Duration", 
-        3 = "Price per Person", 
-        4 = "Min # Walkers", 
-        5 = "Max # Walkers"
+    will read all the data from Guidess.CSV into memory
     '''
-    for line in TOURS_DATA:
-        if line[0] == id_to_update: # searching for the ID
-            line[index_to_update] = value_to_add #setting the new value
-            break # only updating the first instance of line
+    pass
         
-    write_tours_data(TOURS_DATA) # since we update a line we need re-write all the data to the CSV file
-    read_tour_data() # re-reading data so its updated
+# WE DONT NEED update_tour_data I MISSREAD THE TASK
+# def update_tour_data(id_to_update: str, index_to_update: int, value_to_add: str):
+#     '''
+#     Updates tour data
+#     id_to_update = the unique id of the tour
+#     index_to_update = the field u want to update 
+#         0 = "TourID", 
+#         1 = "Destination", 
+#         2 = "Duration", 
+#         3 = "Price per Person", 
+#         4 = "Min # Walkers", 
+#         5 = "Max # Walkers"
+#     '''
+#     for line in TOURS_DATA:
+#         if line[0] == id_to_update: # searching for the ID
+#             line[index_to_update] = value_to_add #setting the new value
+#             break # only updating the first instance of line
+        
+#     write_tours_data(TOURS_DATA) # since we update a line we need re-write all the data to the CSV file
+#     read_tour_data() # re-reading data so its updated
 
-
-def delete_tour_data(id_to_delete:str):
-    '''
-    deletes tour line based on TourID
-    '''
-    for index, line in enumerate(TOURS_DATA): # enumerate lets me get the index and the line value
-        if line[0] == id_to_delete: # searching for the ID
-            del(TOURS_DATA[index])# we found the id so we delete this line
-            break # only delete the first instance of line
-    write_tours_data(TOURS_DATA) # since we deleted a line we need re-write all the data to the CSV file
-    read_tour_data() # re-reading data so its updated
+# WE DONT NEED delete_tour_data I MISSREAD THE TASK
+# def delete_tour_data(id_to_delete:str):
+#     '''
+#     deletes tour line based on TourID
+#     '''
+#     for index, line in enumerate(TOURS_DATA): # enumerate lets me get the index and the line value
+#         if line[0] == id_to_delete: # searching for the ID
+#             del(TOURS_DATA[index])# we found the id so we delete this line
+#             break # only delete the first instance of line
+#     write_tours_data(TOURS_DATA) # since we deleted a line we need re-write all the data to the CSV file
+#     read_tour_data() # re-reading data so its updated
+    
 
 #write_tours_data(TOURS_TEST_DATA)
 write_guides_data(GUIDES_TEST_DATA)
