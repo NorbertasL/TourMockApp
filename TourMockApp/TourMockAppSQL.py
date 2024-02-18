e95f19c287c4ebfc8cf4ada340d1db0a8025236c
Norbertas <nlapenas@gmail.com>
2024-02-18 09:16:58 +0000

    add_row_to created

--- TourMockApp/TourMockAppSQL.py
+++ TourMockApp/TourMockAppSQL.py
@@ -8,12 +8,12 @@ TOURS_TABLE_NAME:str = "tours"
 GUIDES_TABLE_NAME:str = "guides"
 WALKS_TABLE_NAME:str = "walks"
 
-def execure_sql_querry(sql_statment:str, write_querry:bool = False) -> list | bool: # list | bool Python 3.10+ Hinting
+def execure_sql_querry(sql_statment:str, write_querry:bool = False, values:tuple = ()) -> list | bool: # list | bool Python 3.10+ Hinting
     return_value: list | bool = False 
     try:
         with sqlite3.connect(DB_PATH) as conn:
             cursor = conn.cursor()
-            cursor.execute(sql_statment)
+            return_value = cursor.execute(sql_statment, values).rowcount > 0 #rowcount = affected rows
             if write_querry:
                 conn.commit()#commit is only needed for write querries
                 return_value = True # No error so SQL querry was successful 
@@ -56,6 +56,16 @@ walker_count INTEGER
     execure_sql_querry(guides_table_sql, True)
     execure_sql_querry(walks_table_sql, True)  
     
+def add_row_to(table:str, values:dict):
+    
+    sql_querry:str = f"INSERT INTO {table}({','.join(values.keys())}) VALUES({','.join('?'*len(values))})"
+  
+    if execure_sql_querry(sql_querry, True, tuple(values.values())):
+        print(f"{values} data was added to {table}")
+    else:
+        print(f"FAILED to add {values} data to {table}")
+    
+    
 
     
 def dummy_data_interface():
@@ -65,7 +75,11 @@ def dummy_data_interface():
             print(f"{table} TABLE data was DELETED!")
         else:
             print(f"{table} TABLE data DELETION FAILED!")
-        
+    
+    def walks_dummy_data():
+        add_row_to(WALKS_TABLE_NAME, {"guide_ID":5, "tour_ID":7, "walker_count":10})
+        add_row_to(WALKS_TABLE_NAME, {"guide_ID":5, "tour_ID":7, "walker_count":20})
+       
         
     if input("Remake Database with TEST DATA?(Y/N)")[0].upper() == "Y":
         print("Deleting any existing data...")
@@ -74,13 +88,20 @@ def dummy_data_interface():
         delete_table_data("walks")
 
         print("Generating new data...")
-        #TODO
+       
+        walks_dummy_data()
+    print("#"*20)
         
+def user_interface():#TODO
+    pass 
         
         
 
 if TEST_MODE:
     create_tables()
     dummy_data_interface()
+
+user_interface()
+print("App is closed!")
     
   
